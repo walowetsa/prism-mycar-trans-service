08844c71106b62202f9bd7e2bc181bafb5bc6340
walowe <williamlowedev@gmail.com>
2025-08-21 16:48:36 +1000

    initial commit

--- src/app/api/process-calls/route.ts
+++ src/app/api/process-calls/route.ts
@@ -13,7 +13,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: 5432,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: 'gR!eKm^t|iM$2H0TW=3yN.MI,RT9E~.oQ.YUt!mKCT',
   database: process.env.DB_NAME,
 });
 
